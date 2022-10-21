1d7eaeba78904d3143e6fb8386646276ae1356ff
rrauf8178 <rrauf8178@gmail.com>
2022-10-22 03:44:13 +0400

    Created header, and categoryitems components

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import CategoryItem from "./src/components/CategoryItem";
+import Header from "./src/components/Header";
+import Search from "./src/components/Search";
 
 export default function App() {
+  const commonCategories = [
+    {
+      name: "Burger",
+      imageUrl: require("./src/assets/images/burger.png"),
+    },
+    {
+      name: "Pizza",
+      imageUrl: require("./src/assets/images/pizza.png"),
+    },
+    {
+      name: "Dessert",
+      imageUrl: require("./src/assets/images/cake.png"),
+    },
+    {
+      name: "Drinks",
+      imageUrl: require("./src/assets/images/smoothies.png"),
+    },
+    {
+      name: "Burger",
+      imageUrl: require("./src/assets/images/burger.png"),
+    },
+    {
+      name: "Steak",
+      imageUrl: require("./src/assets/images/steak.png"),
+    },
+    {
+      name: "Pasta",
+      imageUrl: require("./src/assets/images/pasta.png"),
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header />
+      <Search />
+
+      <FlatList
+        data={commonCategories}
+        renderItem={({ item, index }) => {
+          return (
+            <CategoryItem
+              name={item.name}
+              imageUrl={item.imageUrl}
+              index={index}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(category) => category.name}
+      />
+
+      {/* <CategoryItem 
+        name='Burger'
+        imageUrl={require("./src/assets/images/burger.png")}
+      /> */}
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
 });
